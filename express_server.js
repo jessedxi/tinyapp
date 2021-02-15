61874bf444512d3cc7ee814aafcf4c6ce8445537
Jesse <jessedaniel@live.ca>
2021-02-15 23:12:21 +0000

    fixed linting errors :)

--- express_server.js
+++ express_server.js
@@ -3,17 +3,17 @@ const app = express();
 const PORT = 8080; // <= Defeault port 8080
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
-function generateRandomString() {
-const randomChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-let result = ""
-for (let i = 0; i < 7; i++) {
-  result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
-}
-return result;
+const generateRandomString = function() {
+  const randomChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < 7; i++) {
+    result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+  }
+  return result;
 }
 
 const urlDatabase = {
@@ -30,15 +30,15 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase};
+  const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.post("/urls", (req, res) => {
   console.log(req.body);
-  const longURL = req.body.longURL
-  let randomID = generateRandomString()
-  urlDatabase[randomID] = longURL
+  const longURL = req.body.longURL;
+  let randomID = generateRandomString();
+  urlDatabase[randomID] = longURL;
   res.redirect("/urls/" + randomID);
 });
 
@@ -54,7 +54,7 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
